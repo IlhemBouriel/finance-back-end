5777fef80950e493da7cb76f8d40415884b7f272
IlhemBouriel <ilhem.bouriel94@gmail.com>
2017-11-20 23:28:22 +0100

    first commit

---
+++ src/main/java/com/example/SpringFinance/SpringFinanceApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFinanceApplication.class, args);
+	}
+}
